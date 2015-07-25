24ca93e2498374a49536b44071970a4635045032
skacns@naver.com <skacns@naver.com>
2015-07-25 15:42:23 +0900

    Service <-> Client init

--- health/app/src/main/java/com/cgy/health/MainActivity.java
+++ health/app/src/main/java/com/cgy/health/MainActivity.java
@@ -1,38 +1,115 @@
 package com.cgy.health;
 
 import android.app.TabActivity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.provider.Settings;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TabHost;
+import android.widget.TextView;
+
+import com.cgy.health.service.BluetoothLeService;
 
 
 public class MainActivity extends TabActivity {
+
+    boolean mIsBound = false;
+    Messenger mService = null;
+    final Messenger mMessenger = new Messenger(new IncomingHandler());
+    Button sendBtn;
+    TextView receiveTextView;
+
+    class IncomingHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothLeService.MSG_SET_INT_VALUE:
+                    //receiveTextView.setText(msg.getData().getString("data"));
+                    break;
+                case BluetoothLeService.MSG_SET_STRING_VALUE:
+                    receiveTextView.setText(msg.getData().getString("str1"));
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            mService = new Messenger(service);
+            //textStatus.setText("Attched.");
+            try {
+                Message msg = Message.obtain(null, BluetoothLeService.MSG_REGISTER_CLIENT);
+                msg.replyTo = mMessenger;
+                mService.send(msg);
+            }
+            catch (RemoteException e) {
+                // In this case the service has crashed before we could even do anything with it
+            }
+        }
+
+        public void onServiceDisconnected(ComponentName className) {
+            // This is called when the connection with the service has been unexpectedly disconnected - process crashed.
+            mService = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        AlertDialog a;
+        sendBtn = (Button)findViewById(R.id.sendBtn);
+        receiveTextView = (TextView)findViewById(R.id.receiveText);
 
+        AlertDialog a;
 
         TabHost tabHost = getTabHost();
         TabHost.TabSpec spec;
 
         spec = tabHost.newTabSpec("health").setIndicator(getString(R.string.exercise_page_tab)).setContent(R.id.tab1);
         tabHost.addTab(spec);
+
+        startBLEService();
+        sendBtn.setOnClickListener(sendBtnListener);
     }
+
+    private View.OnClickListener sendBtnListener = new View.OnClickListener() {
+        public void onClick(View v){
+            //이 부분이 로그상 Timer의 work count 증가 값 정의 하는 부분
+            //현재 Init 을 호출 하고 있지만 추후에 값을 전달해서 세팅 가능하도록 하면됨
+            sendMessageToService(1);
+        }
+    };
 
     @Override
     protected void onStart() {
         super.onStart();
 
         Button button = (Button) this.findViewById(R.id.selectBeaconBtn);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            stopBLEService();
+        }
+        catch (Throwable t) {
+            Log.e("MainActivity", "Failed to unbind from the service", t);
+        }
     }
 
 
@@ -64,4 +141,54 @@ public class MainActivity extends TabActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void startBLEService() {
+        doBindService();
+        startService(new Intent(MainActivity.this, BluetoothLeService.class));
+    }
+
+    public void stopBLEService() {
+        doUnbindService();
+        stopService(new Intent(MainActivity.this, BluetoothLeService.class));
+    }
+
+    void doBindService() {
+        bindService(new Intent(this, BluetoothLeService.class), mConnection, Context.BIND_AUTO_CREATE);
+        mIsBound = true;
+        //textStatus.setText("Binding.");
+    }
+
+    void doUnbindService() {
+        if (mIsBound) {
+            // If we have received the service, and hence registered with it, then now is the time to unregister.
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null, BluetoothLeService.MSG_UNREGISTER_CLIENT);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                }
+                catch (RemoteException e) {
+                    // There is nothing special we need to do if the service has crashed.
+                }
+            }
+            // Detach our existing connection.
+            unbindService(mConnection);
+            mIsBound = false;
+        }
+    }
+
+    private void sendMessageToService(int intvaluetosend) {
+        if (mIsBound) {
+            if (mService != null) {
+                try {
+                    Message msg = Message.obtain(null, BluetoothLeService.MSG_SET_INT_VALUE, intvaluetosend, 0);
+                    msg.replyTo = mMessenger;
+                    mService.send(msg);
+                }
+                catch (RemoteException e) {
+                }
+            }
+        }
+    }
+
 }
--- health/app/src/main/java/com/cgy/health/service/BluetoothLeService.java
+++ health/app/src/main/java/com/cgy/health/service/BluetoothLeService.java
@@ -16,22 +16,19 @@
 
 package com.cgy.health.service;
 
-import java.util.List;
-
+import android.app.NotificationManager;
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothManager;
-import android.bluetooth.BluetoothProfile;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Binder;
+import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Timer;
 
 /**
  * Service for managing connection and data communication with a GATT server hosted on a
@@ -39,241 +36,125 @@ import android.util.Log;
  */
 public class BluetoothLeService extends Service {
 	private final static String TAG = BluetoothLeService.class.getSimpleName();
+    private NotificationManager nm;
+    private Timer timer = new Timer();
+    private int counter = 0, incrementby = 1;
+    private static boolean isRunning = false;
 
-	private BluetoothManager mBluetoothManager;
-	private BluetoothAdapter mBluetoothAdapter;
-	private String mBluetoothDeviceAddress;
-	private BluetoothGatt mBluetoothGatt;
-	private int mConnectionState = STATE_DISCONNECTED;
+    ArrayList<Messenger> mClients = new ArrayList<Messenger>(); // Keeps track of all current registered clients.
+    int mValue = 0; // Holds last value set by a client.
+    public static final int MSG_REGISTER_CLIENT = 1;
+    public static final int MSG_UNREGISTER_CLIENT = 2;
+    public static final int MSG_SET_INT_VALUE = 3;
+    public static final int MSG_SET_STRING_VALUE = 4;
 
-	private static final int STATE_DISCONNECTED = 0;
-	private static final int STATE_CONNECTING = 1;
-	private static final int STATE_CONNECTED = 2;
+    final Messenger mMessenger = new Messenger(new IncomingHandler()); // Target we publish for clients to send messages to IncomingHandler.
 
-	public final static String ACTION_GATT_CONNECTED = "com.example.bluetooth.le.ACTION_GATT_CONNECTED";
-	public final static String ACTION_GATT_DISCONNECTED = "com.example.bluetooth.le.ACTION_GATT_DISCONNECTED";
-	public final static String ACTION_GATT_SERVICES_DISCOVERED = "com.example.bluetooth.le.ACTION_GATT_SERVICES_DISCOVERED";
-	public final static String ACTION_DATA_AVAILABLE = "com.example.bluetooth.le.ACTION_DATA_AVAILABLE";
-	public final static String EXTRA_DATA_RAW = "com.example.bluetooth.le.EXTRA_DATA_RAW";
-	public final static String EXTRA_UUID_CHAR = "com.example.bluetooth.le.EXTRA_UUID_CHAR";
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+    class IncomingHandler extends Handler { // Handler of incoming messages from clients.
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MSG_REGISTER_CLIENT:
+                    mClients.add(msg.replyTo);
+                    break;
+                case MSG_UNREGISTER_CLIENT:
+                    mClients.remove(msg.replyTo);
+                    break;
+                case MSG_SET_INT_VALUE:
+                    incrementby = msg.arg1;
+                    sendMessageToUI(0);
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
+    }
 
-	// Implements callback methods for GATT events that the app cares about.  For example,
-	// connection change and services discovered.
-	private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
-		@Override
-		public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
-			String intentAction;
-			if (newState == BluetoothProfile.STATE_CONNECTED) {
-				intentAction = ACTION_GATT_CONNECTED;
-				mConnectionState = STATE_CONNECTED;
-				broadcastUpdate(intentAction);
-				Log.i(TAG, "Connected to GATT server.");
-				// Attempts to discover services after successful connection.
-				Log.i(TAG, "Attempting to start service discovery:" + mBluetoothGatt.discoverServices());
+    private void sendMessageToUI(int intvaluetosend) {
+        counter += incrementby;
+        for (int i=mClients.size()-1; i>=0; i--) {
+            try {
+                // Send data as an Integer
+                //mClients.get(i).send(Message.obtain(null, MSG_SET_INT_VALUE, counter, 0));
 
-			} else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-				intentAction = ACTION_GATT_DISCONNECTED;
-				mConnectionState = STATE_DISCONNECTED;
-				Log.i(TAG, "Disconnected from GATT server.");
-				broadcastUpdate(intentAction);
-			}
-		}
+                //Send data as a String
+                Bundle b = new Bundle();
+                String str1 = "counter = "+ counter;
+                b.putString("str1", str1);
+                Log.i("SendString", "Send Str1 String" + counter);
+                Message msg = Message.obtain(null, MSG_SET_STRING_VALUE);
+                msg.setData(b);
+                mClients.get(i).send(msg);
 
-		@Override
-		public void onServicesDiscovered(BluetoothGatt gatt, int status) {
-			if (status == BluetoothGatt.GATT_SUCCESS) {
-				broadcastUpdate(ACTION_GATT_SERVICES_DISCOVERED);
-			} else {
-				Log.w(TAG, "onServicesDiscovered received: " + status);
-			}
-		}
+            }
+            catch (RemoteException e) {
+                // The client is dead. Remove it from the list; we are going through the list from back to front so this is safe to do inside the loop.
+                mClients.remove(i);
+            }
+        }
+    }
 
-		@Override
-		public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
-			if (status == BluetoothGatt.GATT_SUCCESS) {
-				broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
-			}
-		}
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(TAG, "Service Started.");
+        //나중에 작업
+        //showNotification();
+        //timer.scheduleAtFixedRate(new TimerTask(){ public void run() {onTimerTick();}}, 0, 100L);
+        isRunning = true;
+    }
 
-		@Override
-		public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
-			broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
-		}
-	};
+    private void showNotification() {
+//        nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+//        // In this sample, we'll use the same text for the ticker and the expanded notification
+//        CharSequence text = getText(R.string.service_started);
+//        // Set the icon, scrolling text and timestamp
+//        Notification notification = new Notification(R.drawable.icon, text, System.currentTimeMillis());
+//        // The PendingIntent to launch our activity if the user selects this notification
+//        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+//        // Set the info for the views that show in the notification panel.
+//        notification.setLatestEventInfo(this, getText(R.string.service_label), text, contentIntent);
+//        // Send the notification.
+//        // We use a layout id because it is a unique number.  We use it later to cancel.
+//        nm.notify(R.string.service_started, notification);
+    }
 
-	private void broadcastUpdate(final String action) {
-		final Intent intent = new Intent(action);
-		sendBroadcast(intent);
-	}
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(TAG, "Received start id " + startId + ": " + intent);
+        return START_STICKY; // run until explicitly stopped.
+    }
 
-	private void broadcastUpdate(final String action, final BluetoothGattCharacteristic characteristic) {
-		final Intent intent = new Intent(action);
-		intent.putExtra(EXTRA_UUID_CHAR, characteristic.getUuid().toString());
+    public static boolean isRunning()
+    {
+        return isRunning;
+    }
 
-		// Always try to add the RAW value
-		final byte[] data = characteristic.getValue();
-		if (data != null && data.length > 0) {
-			intent.putExtra(EXTRA_DATA_RAW, data);
-		}
+    private void onTimerTick() {
+        Log.i("TimerTick", "Timer doing work." + counter);
+        try {
+            counter += incrementby;
+            sendMessageToUI(counter);
 
-		sendBroadcast(intent);
-	}
+        }
+        catch (Throwable t) { //you should always ultimately catch all exceptions in timer tasks.
+            Log.e("TimerTick", "Timer Tick Failed.", t);
+        }
+    }
 
-	public class LocalBinder extends Binder {
-		public BluetoothLeService getService() {
-			return BluetoothLeService.this;
-		}
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (timer != null) {timer.cancel();}
+        counter=0;
+        //nm.cancel(R.string.service_started); // Cancel the persistent notification.
+        Log.i(TAG, "Service Stopped.");
+        isRunning = false;
+    }
 
-	@Override
-	public IBinder onBind(Intent intent) {
-		return mBinder;
-	}
 
-	@Override
-	public boolean onUnbind(Intent intent) {
-		// After using a given device, you should make sure that BluetoothGatt.close() is called
-		// such that resources are cleaned up properly.  In this particular example, close() is
-		// invoked when the UI is disconnected from the Service.
-		close();
-		return super.onUnbind(intent);
-	}
-
-	private final IBinder mBinder = new LocalBinder();
-
-	/**
-	 * Initializes a reference to the local Bluetooth adapter.
-	 *
-	 * @return Return true if the initialization is successful.
-	 */
-	public boolean initialize() {
-		// For API level 18 and above, get a reference to BluetoothAdapter through
-		// BluetoothManager.
-		if (mBluetoothManager == null) {
-			mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-			if (mBluetoothManager == null) {
-				Log.e(TAG, "Unable to initialize BluetoothManager.");
-				return false;
-			}
-		}
-
-		mBluetoothAdapter = mBluetoothManager.getAdapter();
-		if (mBluetoothAdapter == null) {
-			Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * Connects to the GATT server hosted on the Bluetooth LE device.
-	 *
-	 * @param address The device address of the destination device.
-	 *
-	 * @return Return true if the connection is initiated successfully. The connection result
-	 *         is reported asynchronously through the
-	 *         {@code BluetoothGattCallback#onConnectionStateChange(android.bluetooth.BluetoothGatt, int, int)}
-	 *         callback.
-	 */
-	public boolean connect(final String address) {
-		if (mBluetoothAdapter == null || address == null) {
-			Log.w(TAG, "BluetoothAdapter not initialized or unspecified address.");
-			return false;
-		}
-
-		// Previously connected device.  Try to reconnect.
-		if (mBluetoothDeviceAddress != null
-				&& address.equals(mBluetoothDeviceAddress)
-				&& mBluetoothGatt != null) {
-
-			Log.d(TAG, "Trying to use an existing mBluetoothGatt for connection.");
-			if (mBluetoothGatt.connect()) {
-				mConnectionState = STATE_CONNECTING;
-				return true;
-			} else {
-				return false;
-			}
-		}
-
-		final BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
-		if (device == null) {
-			Log.w(TAG, "Device not found.  Unable to connect.");
-			return false;
-		}
-		// We want to directly connect to the device, so we are setting the autoConnect
-		// parameter to false.
-		mBluetoothGatt = device.connectGatt(this, false, mGattCallback);
-		Log.d(TAG, "Trying to create a new connection.");
-		mBluetoothDeviceAddress = address;
-		mConnectionState = STATE_CONNECTING;
-		return true;
-	}
-
-	/**
-	 * Disconnects an existing connection or cancel a pending connection. The disconnection result
-	 * is reported asynchronously through the
-	 * {@code BluetoothGattCallback#onConnectionStateChange(android.bluetooth.BluetoothGatt, int, int)}
-	 * callback.
-	 */
-	public void disconnect() {
-		if (mBluetoothAdapter == null || mBluetoothGatt == null) {
-			Log.w(TAG, "BluetoothAdapter not initialized");
-			return;
-		}
-		mBluetoothGatt.disconnect();
-	}
-
-	/**
-	 * After using a given BLE device, the app must call this method to ensure resources are
-	 * released properly.
-	 */
-	public void close() {
-		if (mBluetoothGatt == null) {
-			return;
-		}
-		mBluetoothGatt.close();
-		mBluetoothGatt = null;
-	}
-
-	/**
-	 * Request a read on a given {@code BluetoothGattCharacteristic}. The read result is reported
-	 * asynchronously through the {@code BluetoothGattCallback#onCharacteristicRead(android.bluetooth.BluetoothGatt, android.bluetooth.BluetoothGattCharacteristic, int)}
-	 * callback.
-	 *
-	 * @param characteristic The characteristic to read from.
-	 */
-	public void readCharacteristic(BluetoothGattCharacteristic characteristic) {
-		if (mBluetoothAdapter == null || mBluetoothGatt == null) {
-			Log.w(TAG, "BluetoothAdapter not initialized");
-			return;
-		}
-		mBluetoothGatt.readCharacteristic(characteristic);
-	}
-
-	/**
-	 * Enables or disables notification on a give characteristic.
-	 *
-	 * @param characteristic Characteristic to act on.
-	 * @param enabled If true, enable notification.  False otherwise.
-	 */
-	public void setCharacteristicNotification(BluetoothGattCharacteristic characteristic, boolean enabled) {
-		if (mBluetoothAdapter == null || mBluetoothGatt == null) {
-			Log.w(TAG, "BluetoothAdapter not initialized");
-			return;
-		}
-		mBluetoothGatt.setCharacteristicNotification(characteristic, enabled);
-	}
-
-	/**
-	 * Retrieves a list of supported GATT services on the connected device. This should be
-	 * invoked only after {@code BluetoothGatt#discoverServices()} completes successfully.
-	 *
-	 * @return A {@code List} of supported services.
-	 */
-	public List<BluetoothGattService> getSupportedGattServices() {
-		if (mBluetoothGatt == null) return null;
-
-		return mBluetoothGatt.getServices();
-	}
 }
